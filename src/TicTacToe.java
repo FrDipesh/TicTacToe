ea137822e968639a1b2cfd0640509796c56ce009
618989 <poudel.618989@swl.k12.oh.us>
2025-11-17 08:56:03 -0500

    InputHelper

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
+import jdk.internal.util.xml.impl.Input;
+
+import java.util.Scanner;
 
 public class TicTacToe {
+    //Create board outside main method
+    static String [][] board = new String[3][3];
+    Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //Game simulation happens here
+
+    }
+
+
+    //Helper methods go here
+    private static void clearBoard(Scanner scan, String prompt){
+        boolean cleanBoard;
+        String userInput;
+
+        System.out.println(prompt);
+        do {
+            userInput = scan.nextLine();
+            if (userInput = scan.nextLine()) {
+                cleanBoard = true;
+            } else {
+                System.out.println();
+            }
+        } while (!cleanBoard);
+        return; userInput;
+
+    }
+
+    public static String getNonZeroLenString(Scanner scan, String prompt) {
+        boolean valid = false;
+        String userInput;
+
+        System.out.println(prompt);
+        do {
+            userInput = scan.nextLine();
+            if (userInput.length() > 0) {
+                valid = true;
+            }
+            else{
+                System.out.println("Error - must enter string of non-zero length");
+            }
+        } while (!valid);
+        return userInput;
+    }
+
 }
